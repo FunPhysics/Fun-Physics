15de6479cfe8ba0b2dbb538fd4856b638caa8bdd
amuammer <amuammer@students.iugaza.edu.ps>
2020-08-17 09:40:25 +0300

    edit articles table

--- data/articles.sql
+++ data/articles.sql
@@ -2,6 +2,7 @@ create table articles (
   id serial primary key,
   title varchar(1000) not null,
   description text not null,
+  reference varchar(1000),
   author_id int,
   foreign key (author_id) references users(id) on delete cascade
 );
